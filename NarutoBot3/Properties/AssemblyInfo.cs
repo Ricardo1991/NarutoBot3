66b30c62abe372fa0da82c3b05db5b147c252074
Ricardo Ribeiro <ricardoribeiro_1991@hotmail.com>
2014-09-05 18:42:28 +0100

    updated assemblyInfo

--- NarutoBot3/Properties/AssemblyInfo.cs
+++ NarutoBot3/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Resources;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("NarutoBot2")]
+[assembly: AssemblyTitle("NarutoBot3")]
 [assembly: AssemblyDescription("bot for a IRC channel")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Ricardo1991")]
-[assembly: AssemblyProduct("NarutoBot2")]
+[assembly: AssemblyProduct("NarutoBot3")]
 [assembly: AssemblyCopyright("Ricardo1991")]
 [assembly: AssemblyTrademark("Ricardo1991")]
 [assembly: AssemblyCulture("")]
@@ -33,6 +33,6 @@ using System.Resources;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("2.0.0.0")]
+[assembly: AssemblyVersion("3.0.0.0")]
+[assembly: AssemblyFileVersion("3.0.0.0")]
 [assembly: NeutralResourcesLanguageAttribute("en")]
